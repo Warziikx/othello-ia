a3299d2e1e6243e50fe6b17c14641dcd098ec57b
Nicolas <durand.nj56@yahoo.fr>
2019-12-17 16:14:37 +0100

    Debut de la grid

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from src.board import Board
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
-
+    board = Board()
+    b = board.new_board(8, 8)
+    newboard = board.get_departure_cells(b)
+    return render_template('index.html', board=b)
 if __name__ == '__main__':
     app.run()
